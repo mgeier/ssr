fdafe08c8f8ae8abe910994ac2e0ef5eb2150493
Matthias Geier <Matthias.Geier@gmail.com>
2020-10-12 14:51:06 +0200

    WIP CI: run only GCC on macos

--- ci/build-deps-macos.sh
+++ ci/build-deps-macos.sh
@@ -20,3 +20,13 @@ make
 sudo make install
 cd ..
 cd ..
+
+git clone https://github.com/vrpn/vrpn.git
+cd vrpn
+mkdir build
+cd build
+cmake -DVRPN_BUILD_JAVA=OFF ..
+make
+sudo make install
+cd ..
+cd ..
